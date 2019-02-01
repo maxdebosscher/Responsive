2743107432cad29d0a19e8dd0910b91db2516fa8
MaxDavinci <99033493@mydavinci.nl>
2019-02-01 09:28:33 +0100

    Nieuw

---
+++ script1.js
@@ -0,0 +1,18 @@
+var dropDown = document.getElementById('ulContainer');
+var dropDown2 = document.getElementById('headerInfo');
+
+function toggle() {
+	if(dropDown.style.display == 'block') {
+		dropDown.style.display = 'none';
+	} else {
+		dropDown.style.display = 'block';
+	}
+}
+
+function toggle2() {
+	if(dropDown2.style.display == 'block') {
+		dropDown2.style.display = 'none';
+	} else {
+		dropDown2.style.display = 'block';
+	}
+}
